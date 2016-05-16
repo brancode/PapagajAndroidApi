c768864df1974135f804d085390a9ff9b90a0738
Branko Dujović <brankod@t-com.me>
2016-05-16 16:21:33 +0200

    Dodat WS za login (vraca korisnika gdje je kartica zadati string)

---
+++ login.php
@@ -0,0 +1,50 @@
+<?php
+ 
+/*
+ * Korisnici
+ */
+ 
+// array for JSON response
+$response = array();
+ 
+// include db connect class
+require_once __DIR__ . '\db_connect.php';
+ 
+// connecting to db
+$db = new DB_CONNECT();
+ 
+// get all products from products table
+$card=$_REQUEST['card'];
+$result = mysql_query("SELECT ime, kartica, region_id " .
+	" FROM korisnik " .
+	" where kartica='$card'; ") or die(mysql_error());
+ 
+// check for empty result
+if (mysql_num_rows($result) > 0) 
+{
+    $response["korisnik"] = array();
+ 
+    while ($row = mysql_fetch_array($result)) 
+	{
+        $product = array(
+          'ime' => utf8_decode($row['ime']),
+          'kartica'=>utf8_decode($row['kartica']),
+		  'region_id'=>utf8_decode($row['region_id']),
+         );
+        array_push($response["korisnik"], $product);
+    }
+    // success
+    //$response["success"] = 1;
+    echo json_encode($response);
+	
+} 
+else 
+{
+    // no products found
+    //$response["success"] = 0;
+    //$response["message"] = "No groups found";
+ 
+    // echo no users JSON
+    echo json_encode($response);
+}
+?>
