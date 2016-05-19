a2a19547a392b8b7f798e07e414b14d7cefa00ed
Branko Dujović <brankod@t-com.me>
2016-05-19 12:04:07 +0200

    Promjena WS za prikaz stolova. Dodate kolone: korisnik_id i kor_ime (ako je sto zauzet) i iznos (iznos na stolu, ako je sto zauzet, inace je 0)

--- sto.php
+++ sto.php
@@ -15,7 +15,16 @@ $db = new DB_CONNECT();
  
 // get all products from products table
 $id=$_REQUEST['id'];
-$result = mysql_query("SELECT sto_id,naziv FROM sto where region_id='$id'") or die(mysql_error());
+$result = mysql_query("SELECT s.sto_id, s.region_id, s.naziv, " .
+	" ifnull(round(sum(p.cijena * (1-p.popust/100) * p.kolicina),2),0) as iznos, " .
+	" pd.korisnik_id, k.ime as kor_ime " .
+    " from sto s  " .                                                                                           
+    " left join temp_porudzbadok pd on (pd.sto_id = s.sto_id) " .                                               
+    " left join temp_porudzba p on (p.temp_porudzbadok_id = pd.temp_porudzbadok_id and p.storno=false) " .      
+    " left join korisnik k on (pd.korisnik_id = k.korisnik_id) " .   
+    " where s.region_id='$id' " .
+    " group by s.sto_id; ") or die(mysql_error());
+ 
  
 // check for empty result
 
@@ -27,7 +36,11 @@ if (mysql_num_rows($result) > 0)
 	{
         $product = array(
           'sto_id' => utf8_decode($row['sto_id']),
+		  'region_id' => utf8_decode($row['region_id']),
           'naziv'=>utf8_decode($row['naziv']),
+		  'iznos' => utf8_decode($row['iznos']),
+		  'korisnik_id' => utf8_decode($row['korisnik_id']),
+		  'kor_ime' => utf8_decode($row['kor_ime']),
          );
         array_push($response["sto"], $product);
     }
